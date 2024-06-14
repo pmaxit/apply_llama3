3f6b14f7452606646eb61cca14eff63fe5d193bf
puneet <puneetgirdhar.in@gmail.com>
2024-06-14 15:33:03 +0000

    adding finetuning doc

---
+++ utils.py
@@ -0,0 +1,62 @@
+import json
+from langchain_core.runnables import RunnableGenerator
+from typing import Iterable
+from langchain_core.messages import AIMessage, AIMessageChunk, HumanMessage
+from langchain.prompts.chat import ChatPromptValue
+
+def extract_json_objects(text):
+    """
+    Extracts JSON objects from a string.
+    
+    Args:
+    text (str): The string containing JSON objects.
+    
+    Returns:
+    list: A list of extracted JSON objects.
+    """
+    json_objects = []
+    start_positions = []
+    brackets_count = 0
+    
+    # Iterate over each character in the string
+    for i, char in enumerate(text):
+        if char == '{':
+            if brackets_count == 0:
+                start_positions.append(i)
+            brackets_count += 1
+        elif char == '}':
+            brackets_count -= 1
+            if brackets_count == 0 and start_positions:
+                start_pos = start_positions.pop()
+                try:
+                    # Extract the substring and parse it as JSON
+                    print('text ' ,text[start_pos:i+1])
+                    json_obj = json.loads(text[start_pos:i+1])
+                    print(json_obj)
+                    json_objects.append(json_obj)
+                except json.JSONDecodeError:
+                    pass  # Ignore parsing errors and continue
+    
+    return json.dumps(json_objects[0])
+
+
+
+
+def streaming_parse(chunks: Iterable[AIMessageChunk]) -> Iterable[str]:
+
+    for chunk in chunks:
+        yield chunk.content
+
+streaming_parse = RunnableGenerator(streaming_parse)
+
+def extract_json(ai_message: AIMessage) -> str:
+    """Parse the AI message."""
+    print(ai_message.content)
+    return extract_json_objects(ai_message.content)
+
+def debug_llm(chat_message: HumanMessage):
+    print('..................'*2)
+    content = chat_message.messages[0].content
+    print(content)
+    print('..................'*2)
+    return ChatPromptValue(messages=[HumanMessage(content=content)])
